9550c6b94818082b09672d1f2bcd68df9385ba23
hoai tran <tranngochoai02051999@gmail.com>
2023-07-06 16:28:56 +0700

    [FEATRE] Get balance

--- app/Context/store.tsx
+++ app/Context/store.tsx
@@ -1,10 +1,12 @@
 "use client";
 
+import { ethers } from "ethers";
 import { createContext, useContext, useEffect, useState } from "react";
 
 const GlobalContext = createContext({
   accounts: [],
   setAccounts: () => [],
+  provider: {},
 });
 
 export const GlobalContextProvider = ({ children }) => {
@@ -12,10 +14,30 @@ export const GlobalContextProvider = ({ children }) => {
   useEffect(() => {
     async function fetchData() {
       // You can await here
-      const getAccounts = await window.ethereum.request({
-        method: "eth_accounts",
-      });
-      setAccounts(getAccounts);
+      try {
+        const getAccounts = await window.ethereum.request({
+          method: "eth_accounts",
+        });
+        const newAccounts = await Promise.all(
+          getAccounts.map(async (address: string) => {
+            const provider = new ethers.JsonRpcProvider(
+              "https://rpc.sepolia.org/"
+            );
+            const balance = await provider.getBalance(
+              ethers.getAddress(address)
+            );
+            return { address, balance: ethers.formatEther(balance) };
+          })
+        );
+        setAccounts(newAccounts);
+
+        // const response = await axios.get('https://api.coingecko.com/api/v3/simple/price?ids=ethereum&vs_currencies=usd');
+        // const data = response.data;
+        // const etherToUSDExchangeRate = data.ethereum.usd;
+        // console.log({etherToUSDExchangeRate});
+      } catch (error) {
+        console.log(error);
+      }
     }
     fetchData();
   }, []);
--- app/page.tsx
+++ app/page.tsx
@@ -3,19 +3,26 @@ import { useEffect, useState } from "react";
 import ConnectWalletAuth from "./components/ConnectWalletAuth";
 import { useGlobalContext } from "./Context/store";
 import { ethers } from "ethers";
+import axios from "axios";
 
 export default function Home() {
   const { accounts } = useGlobalContext();
+  const [balance, setBalance] = useState(0);
+  // useEffect(() => {
+  //   const getBalance = async () => {
+  //     console.log({ accounts });
+  //     const provider = new ethers.JsonRpcProvider("https://rpc.sepolia.org/");
+  //     const balance = await provider.getBalance(accounts[0]);
+  //     setBalance(ethers.formatEther(balance));
 
-  useEffect(() => {
-    const getBalance = async () => {
-      console.log({ accounts });
-      const provider = new ethers.JsonRpcProvider("https://rpc.sepolia.org/");
-      const balance = await provider.getBalance(accounts[0]);
-      console.log({ balance: ethers.formatEther(balance) });
-    };
-    getBalance();
-  }, []);
+  //     // const response = await axios.get('https://api.coingecko.com/api/v3/simple/price?ids=ethereum&vs_currencies=usd');
+  //     // const data = response.data;
+  //     // const etherToUSDExchangeRate = data.ethereum.usd;
+  //     // console.log({etherToUSDExchangeRate});
+      
+  //   };
+  //   getBalance();
+  // }, []);
   return (
     <div>
       {accounts.length === 0 ? (
@@ -24,7 +31,7 @@ export default function Home() {
         <div>
           <div>
             <div>Portfolio value</div>
-            <div className="text-3xl font-bold mt-2">$300.20</div>
+            <div className="text-3xl font-bold mt-2">${accounts[0]?.balance}</div>
           </div>
         </div>
       )}

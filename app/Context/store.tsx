bd819ec8b0804813d6da1e8412ca910535327714
hoai tran <tranngochoai02051999@gmail.com>
2023-07-07 16:34:52 +0700

    [FEATURE] Prepare for get all token

--- app/Context/store.tsx
+++ app/Context/store.tsx
@@ -1,6 +1,6 @@
 "use client";
 
-import { ethers } from "ethers";
+import { Contract, ethers } from "ethers";
 import { createContext, useContext, useEffect, useState } from "react";
 
 const GlobalContext = createContext({
@@ -18,6 +18,26 @@ export const GlobalContextProvider = ({ children }) => {
         const getAccounts = await window.ethereum.request({
           method: "eth_accounts",
         });
+        
+
+        // const abi = [
+        //   "function name() view returns (string)",
+        //   "function decimals() view returns (uint8)",
+        //   "function symbol() view returns (string)",
+        //   "function balanceOf(address a) view returns (uint)",
+        // ];
+        // const provider = new ethers.JsonRpcProvider("https://rpc.sepolia.org/");
+        // const contract = new Contract("dai.tokens.ethers.eth", abi, provider);
+        // // const ba = await contract.balanceOf("0x86f8ce92c8d9b8e2da41832712ff143059277e68");
+        // const ba = "ba"
+        // const name = await contract.name();
+        // const symbol = await contract.symbol();
+        // console.log({name, ba,symbol});
+
+
+
+
+
         const newAccounts = await Promise.all(
           getAccounts.map(async (address: string) => {
             const provider = new ethers.JsonRpcProvider(
@@ -26,6 +46,16 @@ export const GlobalContextProvider = ({ children }) => {
             const balance = await provider.getBalance(
               ethers.getAddress(address)
             );
+
+
+            // const tokenContract = new ethers.Contract(
+            //   "0x514910771AF9Ca656af840dff83E8264EcF986CA",
+            //   tokenAbi,
+            //   provider
+            // );
+            // const balances = await tokenContract.balanceOf(address);
+            // console.log({ balances });
+
             return { address, balance: ethers.formatEther(balance) };
           })
         );

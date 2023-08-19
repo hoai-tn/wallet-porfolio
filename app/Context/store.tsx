7e5e27998948b8e822f893fa150cc56c8b56dd09
Hoai Tran <tranngochoai02051999@gmail.com>
2023-08-20 06:46:01 +0700

    Revert "[TASK] Add moralis"
    This reverts commit c90bbb1b66b34fa0e8df8e4a51b1d658bedc21b9.

--- app/Context/store.tsx
+++ app/Context/store.tsx
@@ -1,14 +1,13 @@
 "use client";
 
-  import { Contract, ethers } from "ethers";
-  import { createContext, useContext, useEffect, useState } from "react";
-  import axios from "axios";
+import { Contract, ethers } from "ethers";
+import { createContext, useContext, useEffect, useState } from "react";
 
-  const GlobalContext = createContext({
-    accounts: [],
-    setAccounts: () => [],
-    provider: {},
-  });
+const GlobalContext = createContext({
+  accounts: [],
+  setAccounts: () => [],
+  provider: {},
+});
 
 export const GlobalContextProvider = ({ children }) => {
   const [accounts, setAccounts] = useState([]);
@@ -19,24 +18,25 @@ export const GlobalContextProvider = ({ children }) => {
         const getAccounts = await window.ethereum.request({
           method: "eth_accounts",
         });
+        
 
-        const abi = [
-          "function name() view returns (string)",
-          "function decimals() view returns (uint8)",
-          "function symbol() view returns (string)",
-          "function balanceOf(address a) view returns (uint)",
-        ];
-        const provider = new ethers.JsonRpcProvider("https://rpc.sepolia.org/");
-        const contract = new Contract(
-          "0x1b44F3514812d835EB1BDB0acB33d3fA3351Ee43",
-          abi,
-          provider
-        );
-        // 0xe2b91697627eb0c7efde8ffb8d7a2ce96d71b92a
-        // const ba = await contract.balanceOf("0x86f8ce92c8d9b8e2da41832712ff143059277e68");
+        // const abi = [
+        //   "function name() view returns (string)",
+        //   "function decimals() view returns (uint8)",
+        //   "function symbol() view returns (string)",
+        //   "function balanceOf(address a) view returns (uint)",
+        // ];
+        // const provider = new ethers.JsonRpcProvider("https://rpc.sepolia.org/");
+        // const contract = new Contract("dai.tokens.ethers.eth", abi, provider);
+        // // const ba = await contract.balanceOf("0x86f8ce92c8d9b8e2da41832712ff143059277e68");
+        // const ba = "ba"
         // const name = await contract.name();
         // const symbol = await contract.symbol();
-        // console.log({  ba, symbol });
+        // console.log({name, ba,symbol});
+
+
+
+
 
         const newAccounts = await Promise.all(
           getAccounts.map(async (address: string) => {
@@ -46,6 +46,7 @@ export const GlobalContextProvider = ({ children }) => {
             const balance = await provider.getBalance(
               ethers.getAddress(address)
             );
+
 
             // const tokenContract = new ethers.Contract(
             //   "0x514910771AF9Ca656af840dff83E8264EcF986CA",
@@ -58,17 +59,12 @@ export const GlobalContextProvider = ({ children }) => {
             return { address, balance: ethers.formatEther(balance) };
           })
         );
-        // setAccounts(newAccounts);
+        setAccounts(newAccounts);
 
         // const response = await axios.get('https://api.coingecko.com/api/v3/simple/price?ids=ethereum&vs_currencies=usd');
         // const data = response.data;
         // const etherToUSDExchangeRate = data.ethereum.usd;
         // console.log({etherToUSDExchangeRate});
-
-
-        const {data} = await axios.get('api');
-        console.log({data});
-        
       } catch (error) {
         console.log(error);
       }
--- app/api/auth/alo/route.ts
+++
@@ -1,8 +0,0 @@
-import { NextResponse } from "next/server";
-
-export async function POST(request: Request) {
-  const res = await request.json(); // res now contains body
-  console.log({ res });
-
-  return NextResponse.json({ data: "hello alo" });
-}
